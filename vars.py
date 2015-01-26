c370da318c305bbc69d256e8899015bdf9a1b202
Umut Bozkurt <umut@hipo.biz>
2015-01-26 14:19:46 +0200

    ask for redis connection instead of url

--- vars.py
+++ vars.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+
+
 PushNotificationURL = ''
 ProfileURL = ''
 RABBIT_URL = ''
 RABBIT_USERNAME = ''
 RABBIT_PASS = ''
-REDIS_URL = ''
+REDIS_CONNECTION = settings.REDIS_CONNECTION
