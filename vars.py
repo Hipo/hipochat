91325fde8330d37f251b19a061b8ac92236626cd
Umut Bozkurt <umut@hipo.biz>
2015-01-26 14:22:35 +0200

    set default redis connection to None

--- vars.py
+++ vars.py
@@ -1,9 +1,7 @@
-from django.conf import settings
-
 
 PushNotificationURL = ''
 ProfileURL = ''
 RABBIT_URL = ''
 RABBIT_USERNAME = ''
 RABBIT_PASS = ''
-REDIS_CONNECTION = settings.REDIS_CONNECTION
+REDIS_CONNECTION = None
